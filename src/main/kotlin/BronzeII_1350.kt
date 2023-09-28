add66b32b9b30fe8adf82082aabb52eef4e5b01f
전지환 <wjswlghks7@ks.ac.kr>
2023-09-28 21:35:55 +0900

    23.09.28 21:35 1350도 미완성

--- src/main/kotlin/BronzeII_1350.kt
+++ src/main/kotlin/BronzeII_1350.kt
@@ -2,12 +2,11 @@ import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)) {
     var files = Array(nextInt()) { nextLong() }
-    var cluster = nextInt()
+    var cluster = nextLong()
     var result = 0L
     for (i in 0 until files.size) {
         if (files[i] == 0L) continue
-        else if (files[i] > cluster) result += cluster * ((files[i] / cluster).toInt() + 1)
-        else result += cluster
+        else result += (files[i] / cluster) + 1L
     }
-    println(result)
+    println(result * cluster)
 }
