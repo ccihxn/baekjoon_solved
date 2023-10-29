43aad8e03a873d58c2fe73bc806b70617a63d154
ccihxn <wjswlghks77@naver.com>
2023-10-29 17:51:40 +0900

    23.10.29 17:51

--- src/main/boj_step15/boj13909.kt
+++ src/main/boj_step15/boj13909.kt
@@ -2,10 +2,12 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
+import kotlin.math.sqrt
 
 fun main() {
     val r = BufferedReader(InputStreamReader(System.`in`))
     val w = BufferedWriter(OutputStreamWriter(System.out))
     val n = r.readLine().toInt()
-    val windows = BooleanArray (n) { false }
+    w.write(sqrt(n.toDouble()).toInt().toString())
+    w.close()
 }
--- src/main/boj_step15/boj17103.kt
+++ src/main/boj_step15/boj17103.kt
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
@@ -8,22 +7,27 @@ fun main() {
     val r = BufferedReader(InputStreamReader(System.`in`))
     val w = BufferedWriter(OutputStreamWriter(System.out))
     val n = r.readLine().toInt()
+    val isPrimes = BooleanArray(1_000_000) { true }
+    isPrimes[0] = false
+    isPrimes[1] = false
+    for (i in 2 until isPrimes.size) {
+        if (isPrimes[i] == false) continue
+        for (j in i * 2 until isPrimes.size step (i)) isPrimes[j] = false
+    }
+    val primes = mutableListOf<Int>()
+    isPrimes.forEachIndexed { index, it -> if (it) primes.add(index) }
     repeat(n) {
         val partition = r.readLine().toInt()
-        val primes = mutableListOf<Int>()
         var count = 0
-        for (prime in 2 .. partition) {
-            primes.add(prime)
-
-        }
         primes.forEach {
-            if (it  >= partition / 2 + 1) return@forEach
-            if (primes.contains(partition - it)) {
-                count++
-                primes.set(primes.indexOf(it), 0)
-            }
+            if (it > partition / 2) return@forEach
+            if (isPrimes[partition - it]) count++
         }
-        w.appendLine("$count")
+        w.appendLine(count.toString())
     }
     w.close()
 }
+
+
+
+
