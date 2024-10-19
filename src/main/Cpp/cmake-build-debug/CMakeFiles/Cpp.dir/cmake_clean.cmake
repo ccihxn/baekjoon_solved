e6a925c5dd8989bf74ac088cef37e5660fc18866
ccihxn <wjswlghks77@naver.com>
2024-10-20 03:52:06 +0900

    24.10.20 03:51

---
+++ src/main/Cpp/cmake-build-debug/CMakeFiles/Cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp.dir/main.cpp.o"
+  "CMakeFiles/Cpp.dir/main.cpp.o.d"
+  "Cpp"
+  "Cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
